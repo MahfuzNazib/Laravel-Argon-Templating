63ce75f4b481b2dfcfbb605527c1afd661518651
MahfuzNazib <nazibmahfuz60@gmail.com>
2021-05-09 11:24:12 +0600

    Setup Authenticate Middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+class Authenticate
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
@@ -12,10 +14,11 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
+    public function handle(Request $request, Closure $next){
+        if(auth('super_admin')->check() || auth('web')->check()){
+            return $next($request);
+        }else{
+            return redirect()->route('login.show');
         }
     }
 }
